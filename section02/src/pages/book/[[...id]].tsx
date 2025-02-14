a4f11a6f459ed37292d282393ec431f7c699fa91
Yunho <tyh1819@gmail.com>
2025-02-14 23:29:45 +0900

    Catch All Segment와 Optional Catch All Segment

---
+++ section02/src/pages/book/[[...id]].tsx
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router";
+
+export default function Page() {
+  const router = useRouter();
+  console.log(router);
+  const { id } = router.query;
+
+  return <h1>Book {id}</h1>;
+}
+
+// Catch All Segment
+// Optional Catch All Segment
